5f4b511c53a58524c8b761ed1428461cb2049f48
NetDanielAlmaguer <67930297+NetDanielAlmaguer@users.noreply.github.com>
2021-05-03 17:38:21 -0500

    Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// 03052021:COMMENTARIO:NDAC: Rutas principales
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
